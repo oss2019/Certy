c99d9b926aa8c0a2ec2242d6bd34ac414038a20a
Tabish Khalid Halim <tkhalim14@gmail.com>
2021-10-15 10:42:02 +0530

    Structure of header created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
 import './App.css';
-
+import Header from "./components/Header/index"
 
 function App() {
   return (
     <div className="App">
         <Router>
+        <Header/>
             <Routes/>
         </Router>
     </div>
